de51e4e626fbaabe82702768c7e8dc304369ab8b
Albert Still <albertjamesstill@gmail.com>
2018-12-13 14:11:46 +1100

    add scss syntax / use async/await / add eslint

--- index.js
+++ index.js
@@ -1,41 +1,50 @@
 #!/usr/bin/env node
+/* eslint-disable no-param-reassign, no-console */
 const postcss = require('postcss');
+const postcssScss = require('postcss-scss');
 const fs = require('fs');
 const { promisify } = require('util');
+
 const readFileAsync = promisify(fs.readFile);
 const writeFileAsync = promisify(fs.writeFile);
 const { camelCase } = require('lodash');
 
-const noSassplugin = postcss.plugin('no-sass', (options = {}) => {
-  return root => {
-    root.walkRules(rule => {
-      // .email-share__form-group => .formGroup
-      const [prefix, postfix] = rule.selector.split('__');
-      rule.selector = `.${camelCase(postfix || prefix)}`;
+const noSassplugin = postcss.plugin('no-sass', () => (root) => {
+  root.walkRules((rule) => {
+    // .email-share__form-group => .formGroup
+    const [prefix, postfix] = rule.selector.split('__');
+    rule.selector = `.${camelCase(postfix || prefix)}`;
 
-      rule.walkDecls(decl => {
-        // color: $fe-brary-colour-neutral-400; => color: var(--fe-brary-colour-neutral-400);
-        if (decl.value[0] === '$') {
-          decl.value = `var(--${decl.value.slice(1)})`
-        }
-      });
+    rule.walkDecls((decl) => {
+      console.log('decl.prop:', decl.prop);
+      // color: $fe-brary-colour-neutral-400; => color: var(--fe-brary-colour-neutral-400);
+      if (decl.value[0] === '$') {
+        decl.value = `var(--${decl.value.slice(1)})`;
+      }
     });
+  });
 
-    root.walkAtRules('extend', rule => {
-      rule.name = 'apply';
-      rule.params = `--${rule.params.split('%')[1]}`;
-    });
-  };
+  root.walkAtRules('extend', (rule) => {
+    rule.name = 'apply';
+    rule.params = `--${rule.params.split('%')[1]}`;
+  });
 });
 
-Promise.all(
-  process.argv.slice(2).map(filePath =>
-    readFileAsync(filePath).then(fileString =>
-      postcss([noSassplugin])
-        .process(fileString, { from: filePath })
-        .then(lazyResult => writeFileAsync(filePath, lazyResult.css)),
-    ),
-  ),
-).then(() => {
+(async () => {
+  const files = process.argv.slice(2);
+
+  const transformFiles = files.map(async (filePath) => {
+    const fileString = await readFileAsync(filePath);
+
+    const lazyResult = await postcss([noSassplugin])
+      .process(fileString, { from: filePath, syntax: postcssScss });
+
+    await writeFileAsync(filePath, lazyResult.css);
+  });
+
+
+  await Promise.all(transformFiles);
+
+  console.log('Finished successfully!');
   process.exit();
-});
+})();
