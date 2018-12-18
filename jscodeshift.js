b3f5ba750296d5e1a8ef152d2ea73fc2d7405542
Albert Still <albertjamesstill@gmail.com>
2018-12-18 17:36:49 +1100

    use jscodeshift instead of babel

--- jscodeshift.js
+++ jscodeshift.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-param-reassign, no-console */
-const { basename } = require('path');
 const { camelCase } = require('lodash');
 
 const STYLES_IMPORT_NAME = 'styles';
@@ -65,24 +64,51 @@ module.exports = (file, api) => {
     });
 
   // import styles
-  const moduleName = `../style/${basename(file.path)}`;
+  const moduleName = `../style/${file.path.split('src/js/')[1]}`;
 
   const collection = root.find(j.ImportDeclaration);
 
-  const target = collection.at(-1).get();
+  // I'd love to simply add to last import but insertAfter causing a line break after last import
+  // and before new styles import so code below gets weird
+  // https://github.com/facebook/jscodeshift/issues/185
+  const relativeImports = collection.filter(path => path.value.source.value.startsWith('.'));
 
-  const importStatement = j.importDeclaration(
+  let lastImportTarget;
+
+  if (relativeImports.length) {
+    lastImportTarget = relativeImports.get();
+  } else {
+    lastImportTarget = collection.at(-1).get();
+  }
+
+  const stylesImportStatement = j.importDeclaration(
     [j.importDefaultSpecifier(j.identifier(STYLES_IMPORT_NAME))],
     j.literal(moduleName),
   );
 
-  // doing this because https://github.com/facebook/jscodeshift/issues/185
-  const reactImportStatement = j.importDeclaration(
-    [j.importDefaultSpecifier(j.identifier('React'))],
-    j.literal('react'),
-  );
+  // e.g import foo from 'foo';
+  const lastImportIsSimpleModule = lastImportTarget.value.specifiers.length === 1
+    && lastImportTarget.value.specifiers[0].type === 'ImportDefaultSpecifier';
 
-  j(target).replaceWith(importStatement).insertBefore(reactImportStatement);
+  // note linter wants relative import last
+  const lastImportIsRelative = lastImportTarget.value.source.value.startsWith('.');
+
+  if (lastImportIsRelative) {
+    j(lastImportTarget).insertBefore(stylesImportStatement);
+  } else if (lastImportIsSimpleModule) {
+    const oldImport = j.importDeclaration(
+      [j.importDefaultSpecifier(j.identifier(lastImportTarget.value.specifiers[0].local.name))],
+      j.literal(lastImportTarget.value.source.value),
+    );
+    j(lastImportTarget)
+      .replaceWith(stylesImportStatement)
+      // even if I use old lastImportTarget.value reference instead of oldImport, issue persists.
+      .insertBefore(oldImport);
+  } else {
+    // this will cause a line break after last import, seems FB strugled with this one too https://github.com/reactjs/react-codemod/blob/96b55a0ea70c7b1a9c64d12b47e523804bb74b22/transforms/__testfixtures__/ReactNative-View-propTypes/default-import-multi-reference.output.js#L4
+    j(lastImportTarget).insertAfter(stylesImportStatement);
+  }
+
 
   return root.toSource({ quote: 'single', trailingComma: true });
 };
